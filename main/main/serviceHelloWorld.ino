e2bcea9606fedb20650e44ac731438f30e5b496a
danboe90 <daniel.boeck@gmx.at>
2019-06-24 18:45:16 +0200

    DOC: added function documentation

--- main/main/serviceHelloWorld.ino
+++ main/main/serviceHelloWorld.ino
@@ -1,3 +1,14 @@
+
+
+
+
+
+
+/**
+ *  @brief  This function represents the task of a heartbeat, offering the user to 
+ *          view over Serial Monitor if the HelTec ESP32-LoRa is in operation.
+ *  @param  pvParameter - not in sue
+ */
 void hello_task(void *pvParameter) {
 
 
